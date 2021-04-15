63aeda00429b4945697c5d28af9bbb2ea84c1b95
ventimiglia <sebaventimiglia23@gmail.com>
2021-04-15 01:59:52 -0300

    first test

---
+++ lpc_chip_175x_6x/Debug/src/ring_buffer.d
@@ -0,0 +1,7 @@
+src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
+ S:\UNLaM\Tecnicas\ Digitales\ II\step-motor-workspace\lpc_chip_175x_6x\inc/ring_buffer.h \
+ S:\UNLaM\Tecnicas\ Digitales\ II\step-motor-workspace\lpc_chip_175x_6x\inc/lpc_types.h
+
+S:\UNLaM\Tecnicas\ Digitales\ II\step-motor-workspace\lpc_chip_175x_6x\inc/ring_buffer.h:
+
+S:\UNLaM\Tecnicas\ Digitales\ II\step-motor-workspace\lpc_chip_175x_6x\inc/lpc_types.h:
